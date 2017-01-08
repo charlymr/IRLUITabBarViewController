ba809e37c8328afeb67d5566d62a0bbab4b4948c
MARTIN Denis <denis@irlmobile.com>
2017-01-08 14:56:19 +0100

    Initial Commit

---
+++ IRLUITabBarViewController/IRLUITabBarViewController.h
@@ -0,0 +1,19 @@
+//
+//  IRLUITabBarViewController.h
+//  IRLUITabBarViewController
+//
+//  Created by Denis Martin on 08/01/2017.
+//  Copyright © 2017 iRLMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRLUITabBarViewController.
+FOUNDATION_EXPORT double IRLUITabBarViewControllerVersionNumber;
+
+//! Project version string for IRLUITabBarViewController.
+FOUNDATION_EXPORT const unsigned char IRLUITabBarViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRLUITabBarViewController/PublicHeader.h>
+
+
